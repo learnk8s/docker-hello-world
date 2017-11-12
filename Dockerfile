f8a6f2ab93045c15eacb25fc064c29aa9ea8224c
Daniele Polencic <daniele.polencic@gmail.com>
2017-11-12 19:09:32 +0000

    demo

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node
+FROM node:8-alpine
 WORKDIR /app
 COPY . .
 RUN yarn install
--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080;
+const Redis = require('ioredis')
+
+const VERSION = '2.0.0'
+
+const redis = new Redis()
+
+redis.on('error', () => {})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/version', (req, res) => {
-  res.send(process.env.VERSION || 'No version')
+  res.send(VERSION)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+app.get('/healthz', (req, res) => {
+  if (redis.status === 'ready') {
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(500)
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
 })
